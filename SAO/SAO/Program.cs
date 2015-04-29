245cd343e800212a4f015651ed3fbe40d6ded011
Radosław Chamot <radoslaw.chamot@schibsted.pl>
2015-04-29 11:18:11 +0200

    add c# files

---
+++ SAO/SAO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SAO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var routs = new Routes("0:10,1:40,2:20,3:10,4:40,5:20,6:60,7:50,8:10,9:20,10:10");
+            var lines = new List<Line>
+            {
+                new Line("0:0,1,2,7,8,9,10"),
+                new Line("1:4,6,8,9,10"),
+                new Line("2:3,7,6,5"),
+                new Line("3:0,1,4,5")
+            };
+            const int numberOfBuses = 10;
+            const int busCapacity = 5;
+            const int numberOfIterations = 2000;
+            const int poolOfSpecimens = 100;
+
+            var solution = new Solution(routs, lines, numberOfBuses, busCapacity, numberOfIterations, poolOfSpecimens);
+            solution.Execute();
+            Console.WriteLine(solution.BestResult.Value);
+            foreach (var p in solution.BestResult.Distribution)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SAO/SAO/Specimen.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAO
+{
+    public class Specimen
+    {
+
+        public List<int> Distribution { get; private set; }
+        public int Value { get; private set; }
+        public Routes Routes { get; private set; }
+        public List<Line> Lines { get; private set; }
+        public int NumberOfBuses { get; private set; }
+        public int BusCapacity { get; private set; }
+
+        public Specimen(Routes routes, List<Line> lines, int numberOfBuses, int busCapacity)
+        {
+            Routes = routes;
+            Lines = lines;
+            NumberOfBuses = numberOfBuses;
+            BusCapacity = busCapacity;
+            SetRandomDistribution();
+            CalculateSpecimentValue();
+        }
+
+        public Specimen(Specimen toClone)
+        {
+            Routes = toClone.Routes;
+            Lines = toClone.Lines;
+            NumberOfBuses = toClone.NumberOfBuses;
+            BusCapacity = toClone.BusCapacity;
+            CopyDistribution(toClone);
+            CalculateSpecimentValue();
+        }
+
+        private void SetRandomDistribution()
+        {
+            Distribution = new List<int>();
+            var random = new Random();
+            int[] leftSpace = {Lines.Count};
+            int[] sum = {0};
+
+            foreach (var toAdd in Lines.Select(line => random.Next(1, (NumberOfBuses - sum[0]) / leftSpace[0])))
+            {
+                Distribution.Add(toAdd);
+                sum[0] += toAdd;
+                leftSpace[0] -= 1;
+            }
+
+            for (var i = 0; i < Lines.Count; i++)
+            {
+                var toSwap = random.Next(0, Lines.Count - 1);
+                var tmp = Distribution[toSwap];
+                Distribution[toSwap] = Distribution[i];
+                Distribution[i] = tmp;
+            }
+        }
+
+        private void CopyDistribution(Specimen toClone)
+        {
+            Distribution = new List<int>();
+            foreach (var value in toClone.Distribution)
+            {
+                Distribution.Add(value);
+            }
+        }
+
+        private void CalculateSpecimentValue()
+        {
+            var result = Routes.CreateTemporaryCopyOfRoutesValues();
+
+            for (var i = 0; i < Lines.Count; i++)
+            {
+                var value = Distribution[i]*BusCapacity;
+                var linesTrace = Lines[i].Trace;
+                foreach (var pointOnTrace in linesTrace)
+                {
+                    var oldValue = result[pointOnTrace];
+                    result[pointOnTrace] = oldValue - value;
+                }
+            }
+            Value = result.Sum(val => Math.Abs(val));
+        }
+
+        public void MutateByChangingPlaces()
+        {
+            var random = new Random();
+            var numberOfChanges = random.Next(0, Lines.Count);
+            for (var i = 0; i < numberOfChanges; i++)
+            {
+                var first = random.Next(1, Lines.Count - 1);
+                var second = random.Next(1, Lines.Count - 1);
+                var temp = Distribution[first];
+                Distribution[first] = Distribution[second];
+                Distribution[second] = temp;
+            }
+            CalculateSpecimentValue();
+        }
+
+        public void Mutate()
+        {
+            var random = new Random();
+            var toBeChanged = Lines.Select(line => random.Next(0, 100)).Select(rolled => rolled < 20 ? 1 : 0).ToList();
+
+            for (var i = 0; i < Lines.Count; i++)
+            {
+                var busses = Distribution.Sum();
+                var max = NumberOfBuses - busses;
+                var min = -(Distribution[i] - 1);
+                if (toBeChanged[i] == 1 && min < max)
+                {
+                    Distribution[i] += random.Next(min, max);
+                }
+            }
+        }
+    }
+}
